b8fd40d993105fe0d3a092157f75df403743cb3a
Rafay <abdurrafay073@gmail.com>
2020-07-21 16:12:35 -0700

    dived into restful routing. added show route

--- app.js
+++ app.js
@@ -21,13 +21,13 @@ var campgroundSchema = new mongoose.Schema({
 
 var Campground = mongoose.model('Campground', campgroundSchema)
 // temp
-Campground.create({
-  name: 'Granite Hill',
-  image: 'https://p0.pikrepo.com/preview/170/362/rock-with-pine-trees-at-quincy-bluff-wisconsin-thumbnail.jpg',
-  description: 'An escape like none other. It\'s just you, granite and an open sky'
-}, (err, campground)=>{
-  err ? console.log(`error: ${err}`) : console.log(`Campground created: ${campground}`)
-})
+// Campground.create({
+//   name: 'Granite Hill',
+//   image: 'https://p0.pikrepo.com/preview/170/362/rock-with-pine-trees-at-quincy-bluff-wisconsin-thumbnail.jpg',
+//   description: 'An escape like none other. It\'s just you, granite and an open sky'
+// }, (err, campground)=>{
+//   err ? console.log(`error: ${err}`) : console.log(`Campground created: ${campground}`)
+// })
 // ======================================================
 //                       Get Routes
 // ======================================================
@@ -39,7 +39,7 @@ app.get('/', (req, res)=>{
 app.get('/campgrounds', (req, res)=>{
   // Get all campgrounds from DB
   Campground.find({}, (err, allCampgrounds)=>{
-    err ? console.log(`Error: ${err}`) : res.render('campgrounds', {campgrounds: allCampgrounds})
+    err ? console.log(`Error: ${err}`) : res.render('index', {campgrounds: allCampgrounds})
   })
 })
 
@@ -48,10 +48,11 @@ app.get('/campgrounds/new', (req, res) => {
   res.render('new')
 })
 
-// SHOW - show info on specific campground (must come after NEW because of the catch all)
+// SHOW - shows more info about one campground
 app.get('/campgrounds/:id', (req, res) => {
-
-  res.send('THIS WILL BE THE SHOW PAGE ONE DAY')
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    err ? console.log(`error: ${err}`) : res.render('show', {campground: foundCampground})
+  })
 })
 
 app.get('*', (req, res)=>{
@@ -64,7 +65,8 @@ app.get('*', (req, res)=>{
 app.post('/campgrounds', (req, res)=>{
   const name = req.body.name
   const image = req.body.image
-  const newCampground = {name: name, image: image}
+  const desc = req.body.description
+  const newCampground = {name: name, image: image, description: desc}
   // Create a new campground and save to DB
   Campground.create(newCampground, (err, campground)=>{
     err ? console.log(`Error: ${err}`) : res.redirect('campgrounds')
